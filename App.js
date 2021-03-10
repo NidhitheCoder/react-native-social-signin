5d9c58ad960ed494d2438801215be5f479e6c972
NidhitheCoder <nidhpv@gmail.com>
2021-03-10 22:19:56 +0530

    initial commit

--- App.js
+++ App.js
@@ -23,8 +23,25 @@ import {
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import { GoogleSignin,GoogleSigninButton} from 'react-native-google-signin';
+
+GoogleSignin.configure({
+  webClientId: '1094604138839-veb1g10qufd6bnkp446qmqtfkuo1htdf.apps.googleusercontent.com', 
+  offlineAccess: true,
+});
 
 const App: () => React$Node = () => {
+
+  signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      this.setState({ userInfo });
+    } catch (error) {
+    console.log(error)
+    }
+  }
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
@@ -33,6 +50,13 @@ const App: () => React$Node = () => {
           contentInsetAdjustmentBehavior="automatic"
           style={styles.scrollView}>
           <Text>Heyyy</Text>
+
+          <GoogleSigninButton
+          style={{ width: 192, height: 48 }}
+          size={GoogleSigninButton.Size.Wide}
+          color={GoogleSigninButton.Color.Dark}
+          onPress={signIn}
+           />
          </ScrollView>
       </SafeAreaView>
     </>
@@ -42,6 +66,7 @@ const App: () => React$Node = () => {
 const styles = StyleSheet.create({
   scrollView: {
     backgroundColor: Colors.lighter,
+    padding:10
   }
 });
 
